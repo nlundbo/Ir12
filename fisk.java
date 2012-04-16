1af1ce11bdd91b31f59a89b3bb8043b96245372d
Niklas Lundborg <nlundbo@csc.kth.se>
2012-04-16 11:01:10 +0200

    fixat lite mer

--- fisk.java
+++ fisk.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
+
 public static void main(String args[]){
-	print(lool)
-=======
-main(){
-	System.println(lool)
->>>>>>> e813e958136043d460fc084d2f2502e4707d6262
+	System.out.println(lool)
 }
