0a4e799b915f86133a1a6ebd0e125efd9e583de5
Niklas Lundborg <nlundbo@kth.se>
2012-04-26 12:44:34 +0200

    broken StopWords.java

---
+++ TfIdf/ir/StopWords.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+import ir.SimpleTokenizer;
+
+public class StopWords{
+
+	public static HashMap<String,Integer> counter = new HashMap<String,Integer>();
+
+	
+	public StopWords(){}
+
+	public static void readData(String dir){
+		File dokDir = new File( dir);		
+		processFiles(dokDir);
+	}
+
+    public static void processFiles( File f ) {
+		if ( f.canRead() ) {
+		    if ( f.isDirectory() ) {
+				String[] fs = f.list();			
+				if ( fs != null ) {
+			    	for ( int i=0; i<fs.length; i++ ) {
+						processFiles( new File( f, fs[i] ));
+			    	}
+				}
+		    } else {
+				System.err.println( "Counting words: " + f.getPath() );					
+				try {			
+				    Reader reader = new FileReader( f );
+				    SimpleTokenizer tok = new SimpleTokenizer( reader );
+				    
+		
+				    while ( tok.hasMoreTokens() ) {
+						//.add(tok.nextToken());
+				    }
+				    parseTokenizedDocument(strs);
+				    reader.close();
+				}
+				catch ( IOException e ) {
+				    e.printStackTrace();
+				}
+		    }
+		}
+    }
+
+
+
+	public static final void main(String [] args){
+		readData("../files/1000/");
+
+	}
+}
