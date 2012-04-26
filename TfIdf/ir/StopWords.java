816e9f8921dc952982e0ecfcd2fa5176736351fb
Niklas Lundborg <nlundbo@csc.kth.se>
2012-04-26 14:04:17 +0200

    Fixed StopWords, now outputs to stdout the top K words.

--- TfIdf/ir/StopWords.java
+++ TfIdf/ir/StopWords.java
@@ -4,17 +4,17 @@ import ir.SimpleTokenizer;
 
 public class StopWords{
 
-	public static HashMap<String,Integer> counter = new HashMap<String,Integer>();
-
+	public  HashMap<String,Integer> counter = new HashMap<String,Integer>();
+	public  static final int NUM_STOP_WORDS = 30;
 	
 	public StopWords(){}
 
-	public static void readData(String dir){
+	public  void readData(String dir){
 		File dokDir = new File( dir);		
 		processFiles(dokDir);
 	}
 
-    public static void processFiles( File f ) {
+    public  void processFiles( File f ) {
 		if ( f.canRead() ) {
 		    if ( f.isDirectory() ) {
 				String[] fs = f.list();			
@@ -29,11 +29,17 @@ public class StopWords{
 				    Reader reader = new FileReader( f );
 				    SimpleTokenizer tok = new SimpleTokenizer( reader );
 				    
-		
+					Integer tmp =0;
+					String token="";
 				    while ( tok.hasMoreTokens() ) {
-						//.add(tok.nextToken());
+				    	token = tok.nextToken();
+				    	tmp = counter.get(token);
+				    	if(tmp==null)tmp=new Integer(0);
+				    	tmp++;
+
+						counter.put(token,tmp);
 				    }
-				    parseTokenizedDocument(strs);
+				    
 				    reader.close();
 				}
 				catch ( IOException e ) {
@@ -43,10 +49,61 @@ public class StopWords{
 		}
     }
 
+    public LinkedList<Word> getStopWords(){
+    	
+    	Set<String> ks = counter.keySet();
+		LinkedList<Word> ll = new LinkedList<Word>();
+
+		for(String s : ks)
+			ll.add( new Word(s, counter.get(s)) );
+
+		Collections.sort(ll);
+		return ll;
+    }
 
 
 	public static final void main(String [] args){
-		readData("../files/1000/");
+		StopWords s = new StopWords();
+		s.readData("../files/1000/");
+		s.readData("../files/2000/");
+		s.readData("../files/3000/");
+		s.readData("../files/4000/");
+		s.readData("../files/5000/");
+		s.readData("../files/6000/");
+		s.readData("../files/7000/");
+		s.readData("../files/8000/");
+		s.readData("../files/9000/");
+		s.readData("../files/10000/");
+		
+				
+		LinkedList<Word> ll = s.getStopWords();
+		for(int i =0; i< NUM_STOP_WORDS; ++i){
+			System.out.println(ll.pop());
+		}
 
+	}
+
+
+	private class Word implements Comparable<Word> {
+		public String word;
+		public Integer n;
+
+		public Word() {
+			word = "";
+			n = new Integer(1);
+		}
+
+		public Word(String str, Integer n) {
+			word = str;
+			this.n = n;
+		}
+
+		public int compareTo(Word o) {
+			return o.n.compareTo(n); //reversed.
+		}
+		@Override
+		public String toString(){
+			return "[ " + word + ", " + n + "]";
+		}
 	}
 }
